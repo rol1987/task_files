f4ae60d7008cd2c6bf636df1e9e1c525f253aa4e
sergey korolev <rol1987@yandex.ru>
2022-12-07 22:25:15 +0300

    Сделал задание №3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Задача №1
+
 import os
 current = os.getcwd()
 folder_name = "2.4.files"
@@ -12,7 +14,6 @@ with open(full_path, "r", encoding = "utf8") as file:
             continue
         else:
             dish.append(line.strip())
-    
     # ПЕРЕБОР ОЧИЩЕННОГО СПИСКА
     master_dict = {}
     cook_dict = {}
@@ -35,7 +36,9 @@ with open(full_path, "r", encoding = "utf8") as file:
         master_dict[dish_name] = cooc_list         
 print(master_dict)
 print()
+file.close()
 
+# Задача №2
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list_by_dishes = {}
@@ -49,5 +52,43 @@ def get_shop_list_by_dishes(dishes, person_count):
                     shop_list["quantity"] = int(val["quantity"]) * person_count
                     shop_list_by_dishes[ingredient_name] = shop_list
     print(shop_list_by_dishes)
-            
+
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 3)
+
+# Задача №3
+current = os.getcwd()
+folder_name = "2.4.files\sorted"
+
+
+    
+file_list = []
+
+for root, dirs, files in os.walk(os.path.join(current, folder_name)):  
+    for filename in files:
+        file_list.append(filename)
+
+dict_line_in_files = {}
+
+for file_name in file_list:
+    full_path = os.path.join(current, folder_name, file_name)
+    with open(full_path, "r", encoding = "utf8") as file:
+        dict_line_in_files[file_name] = len(file.readlines())
+        sorted_tuple = sorted(dict_line_in_files.items(), key=lambda x: x[1])
+        dict_line_in_files = dict(sorted_tuple)
+    file.close()
+
+print(dict_line_in_files)
+
+result_file_name = "result.txt"
+result_full_path = os.path.join(current, result_file_name)
+with open(result_full_path, "a", encoding = "utf8") as result_file:
+    for file_key, count_line in dict_line_in_files.items():   
+        full_path = os.path.join(current, folder_name, file_key)
+        with open(full_path, "r", encoding = "utf8") as sorted_file:
+            result_file.write(f"{file_key}\n")
+            result_file.write(f"{count_line}\n")
+            for line in sorted_file:
+                result_file.write(f"{line}\n")
+result_file.close()
+
+
